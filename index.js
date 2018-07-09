5286293c6e5124ad94190c0f0e6fa501c72c45ba
Varun Malik <varun1malik@gmail.com>
2018-07-10 00:34:36 +0530

    english to braille assiognment completed

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
  * in english language to that in the equivalent braille
  * code.
  */
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
+const englishBrailleMap = new Map(englishToBrailleLiteralSet);
+
+function braileconvert() {
+  let brailleString = '';
+  const input = document.getElementById('sourceLangText').value;
+  for (let i = 0; i < input.length; i += 1) {
+    brailleString += englishBrailleMap.get(input[i]);
+  }
+  document.getElementById('targetLangText').innerHTML = brailleString;
+}
+document.getElementById('btnConvertEnglishToBraille').addEventListener('click', braileconvert);
